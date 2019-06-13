f4c5b5dfc78a7dc3cf0775fa855fdb61977e0092
johnanwar <johnanwar30@gmail.com>
2019-06-13 16:22:06 +0300

    FFF

--- js/main.js
+++ js/main.js
@@ -27,12 +27,25 @@ var mw3id = document.querySelectorAll("#mwaaid")
     mw3id.forEach((el)=>{
              el.addEventListener("click", function(e){
              if (e.target.classList.contains("del")){
-             e.target.parentNode.parentNode.remove();
-    }
+             if (confirm('هل انت متاكد ؟')){
+               e.target.parentNode.parentNode.remove();
+               alertDelete();
+                }
+            }
 
     })
    
 });
+var alertDelete = function() {
+    $(".alert").animate({right: "40px"})
+    $(".alert").delay(3000);
+	$(".alert").animate({right:'-194px'},700);
+    // $(".alert").fadeOut(700);
+					
+
+}
+
+
 
 // booking page 
 
@@ -55,4 +68,62 @@ var mw3id = document.querySelectorAll("#mwaaid")
 
 // select2 library
 
+ $(".hary1").each(function(){
+     $(this).select2({})
+ }) ;
+ 
+
+
+
+
+// calender 
+
+
+
+$(document).ready(function () {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay'
+        },
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2019-06-15'
+            },
+            {
+                title: 'Long Event',
+                start: '2019-6-20',
+                end: '2019-06-30'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2019-06-23T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2019-01-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2019-06-25',
+                end: '2019-06-26'
+            },
+          
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2019-06-28'
+            }
+        ]
+    });
+
+});
 
